835fd2799246be11697010ccbd3a3d150130faf9
innnsomnia <art1xd@yandex.ru>
2024-12-02 19:52:09 +0300

    хук для получения постов

--- src/API/postService.js
+++ src/API/postService.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-export default class PostService {
-  static async getAll() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    return response.data
-  }
-}
+// export default class PostService {
+//   static async getAll() {
+//     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+//     return response.data
+//   }
+// }
--- src/Hooks/usePosts.tsx
+++ src/Hooks/usePosts.tsx
@@ -1,13 +1,22 @@
 import { useQuery } from '@tanstack/react-query'
-import PostService from '../API/postService'
+// import PostService from '../API/postService'
+import axios from 'axios'
+import { useState } from 'react'
+
+const postService = async (page = 0) => {
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${page + 1}&_limit=10`)
+
+  return response.data
+}
 
 export const UsePosts = () => {
+  const [page, setPage] = useState(0)
   const {
     data: posts = [],
     isLoading,
     isError,
     error,
-  } = useQuery({ queryKey: ['posts'], queryFn: PostService.getAll, retry: false })
-
+  } = useQuery({ queryKey: ['posts', page], queryFn: () => postService(page), retry: false })
+  console.log(page, 'page в usePosts')
   return { posts, isLoading, isError, error }
 }
